894f9434f69abb01823f5a2f4d27279abd197b21
Qineria <haochinnate@gmail.com>
2020-01-07 12:55:53 +0800

    section 32 use DTO to replace string argument, learn how to debug, POST to create user and debug

--- TestDotNetApp.API/Controllers/AuthController.cs
+++ TestDotNetApp.API/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TestDotNetApp.API.Data;
+using TestDotNetApp.API.Dtos;
 using TestDotNetApp.API.Models;
 
 namespace TestDotNetApp.API.Controllers
@@ -17,23 +18,24 @@ namespace TestDotNetApp.API.Controllers
 
         //
         [HttpPost("register")]
-        public async Task<IActionResult> Register(string username, string password)
+        // public async Task<IActionResult> Register(string username, string password) // replace with DTO argument
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
             // validate request
 
-            username = username.ToLower();
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
-            if (await _repo.UserExists(username))
+            if (await _repo.UserExists(userForRegisterDto.Username))
             {
                 return BadRequest("Username already exists!!");
             }
 
             var userToCreate = new User
             {
-                UserName = username,
+                UserName = userForRegisterDto.Username,
             };
 
-            var createdUser = await _repo.Register(userToCreate, password);
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
             return StatusCode(201);
         }
