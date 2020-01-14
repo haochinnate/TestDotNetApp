8e049de43a34b39790cf04ad08c9c5d4c40407da
Qineria <haochinnate@gmail.com>
2020-01-14 13:02:53 +0800

    login method and create token

--- TestDotNetApp.API/Controllers/AuthController.cs
+++ TestDotNetApp.API/Controllers/AuthController.cs
@@ -1,5 +1,11 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using TestDotNetApp.API.Data;
 using TestDotNetApp.API.Dtos;
 using TestDotNetApp.API.Models;
@@ -11,9 +17,11 @@ namespace TestDotNetApp.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository repo)
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
             _repo = repo;
+            _config = config;
         }
 
         // http://localhost:5000/api/auth/register
@@ -39,11 +47,49 @@ namespace TestDotNetApp.API.Controllers
             return StatusCode(201);
         }
 
-        // [HttpPost("login")]
-        // public async Task<ActionResult> Login(UserForLoginDto userForLoginDto)
-        // {
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            // check is user exist
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
+            if (userFromRepo == null)
+            {
+                return Unauthorized();
+            }
 
-        // }
+            #region Build a token
+    
+            // build a token that return to user
+            // this token contains two Claims (Id, Username)
+            var claims = new []
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.UserName),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.
+                GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            // token descripter
+            var tokenDescripter = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = credentials
+            };
+
+            // token handler(to create token based on token descripter)
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescripter);
+            #endregion
+
+            return Ok(new {
+                token = tokenHandler.WriteToken(token)
+            });
+        } 
     }
 }
---
+++ TestDotNetApp.API/Dtos/UserForLoginDto.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TestDotNetApp.API.Dtos
+{
+    public class UserForLoginDto
+    {
+        // [Required]
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
