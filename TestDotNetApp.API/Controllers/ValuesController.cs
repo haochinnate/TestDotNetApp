ded9244a2a5536dcaa1edc08ba6c9feb745f9320
Qineria <haochinnate@gmail.com>
2020-01-16 12:55:57 +0800

    add authentication and related packeages

--- TestDotNetApp.API/Controllers/ValuesController.cs
+++ TestDotNetApp.API/Controllers/ValuesController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TestDotNetApp.API.Data;
@@ -9,6 +10,7 @@ using TestDotNetApp.API.Data;
 namespace TestDotNetApp.API.Controllers
 {
     // http:localhost:5000/api/values
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -32,6 +34,7 @@ namespace TestDotNetApp.API.Controllers
         }
 
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
--- TestDotNetApp.API/Startup.cs
+++ TestDotNetApp.API/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using TestDotNetApp.API.Data;
 
 namespace TestDotNetApp.API
@@ -34,6 +37,19 @@ namespace TestDotNetApp.API
             // service is create once per request with in scope?
             // inject to controller
             services.AddScoped<IAuthRepository, AuthRepository>();
+
+            // specify authentication scheme
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+                            .GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +68,8 @@ namespace TestDotNetApp.API
             // the order is important
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
+            // add Authentication before Authorization 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
