f448f90d0cf61ae2ee0dea2c09de9aade9eeeb57
Qineria <haochinnate@gmail.com>
2020-03-09 12:58:02 +0800

    add auth service, not yet test it

---
+++ TestDotNetApp-SPA/src/app/_services/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+// inject things to service, service is not component which is injectable by default
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  baseUrl = 'http://localhost:5000/api/auth/';
+
+constructor(private http: HttpClient) { }
+
+login(model: any) {
+  return this.http.post(this.baseUrl + 'login', model)
+    .pipe( // post will return Observable<object>, login method return the "token"
+      map((response: any) => {
+        // see AuthController.cs login method
+        const user = response;
+        if (user) {
+          localStorage.setItem('token', user.token);
+        }
+      })
+    );
+}
+
+}
---
+++ TestDotNetApp.API/doc/note.md
@@ -0,0 +1,18 @@
+### Section 4:
+
+* 在 app.module.ts 中 import FormsModule
+  > import {FormsModule} from '@angular/forms';
+
+* template form:
+<form #loginForm="ngForm" class="...">
+input 項目裡面 則用 [(ngModel)]="model.username" 來binding
+(ngSubmit)="login()" 指定 event 跟要執行的 function 
+
+* [] 是單向binding, 改 view model 會更新view的值 ? 
+  [()] 是雙向binding?
+
+* <pre> 可以放一些不合法的值時候的樣式
+
+* testdotnetapp.api 執行 dotnet watch run 
+
+* testdotnetapp-spa 執行 ng serve
